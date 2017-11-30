a3672d385aca05167f31bb741bc89f14cadf6981
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-11-30 11:21:20 +1100

    Add error handling for quote

--- src/App.js
+++ src/App.js
@@ -3,40 +3,37 @@ import './App.css';
 import StockInfo from './components/StockInfo'
 import { loadQuoteForStock } from './api/iex'
 
-// loadQuoteForStock('nflx')
-//   .then(quote => {
-//     console.log('Loaded Netflix', quote)
-//   })
-
 class App extends Component {
   state = {
+    erro: null,
     quote: null
-    // {
-    //   symbol: "NTFX",
-    //   companyName: "Netflix Inc.",
-    //   primaryExchange: "Nasdaq Global Select",
-    //   latestPrice: 188.15,
-    //   latestSource: "Close",
-    //   week52High: 204.38,
-    //   week52Low: 113.95
-    // }
   }
 
   // The first time our component is rendered
   // this method is called
   componentDidMount() {
-    loadQuoteForStock('nflx')
+    loadQuoteForStock('dddd')
       .then(quote => {
         this.setState({ quote: quote })
+      })
+      .catch(error => {
+        if (error.response.status === 404) {
+          error = new Error('The stock symbol does not exist')
+        }
+        this.setState({ error: error })
+        console.error('Error loading quote', error)
       })
   }
 
   render() {
-    const { quote } = this.state
+    const { error, quote } = this.state
 
     return (
       <div className="App">
         <h1>Wolf of React</h1>
+        {
+          error && <p>{error.message}</p>
+        }
         {quote ? (
           <StockInfo
             { ...quote} // Does the same as below
